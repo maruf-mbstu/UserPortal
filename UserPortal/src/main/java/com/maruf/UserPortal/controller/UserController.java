6168c8b2e94f9c63683c53ceff2d71132497a8e1
maruf-mbstu <maruf.mbstu@gmail.com>
2020-08-07 22:13:08 +0600

    Upload with Ajax!

--- UserPortal/src/main/java/com/maruf/UserPortal/controller/UserController.java
+++ UserPortal/src/main/java/com/maruf/UserPortal/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +22,7 @@ public class UserController {
 
 	@Autowired
 	UserDao dao;
-
+    
 	HttpSession session;
 	String projecturl = "http://localhost:8080/UserPortal/";
 
